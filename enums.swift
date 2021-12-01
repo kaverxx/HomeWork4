06729f230496f064593b63fb611d029a905eced4
kaverxx <95378687+kaverxx@users.noreply.github.com>
2021-12-01 23:02:41 +0300

    Add files via upload

---
+++ enums.swift
@@ -0,0 +1,11 @@
+enum Trailer {
+    case have
+    case haveNot
+}
+
+enum BackSeat {
+    case have
+    case haveNot
+}
+
+//TO:DO Для практики потом напишу здесь больше энамов сам, а для ДЗ думаю хватит и этого
